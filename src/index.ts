a56881596bd82de4d293748cdbcd23d5bb1d502a
Frank Nolasco Velasquez <49081234+FrankNolasco@users.noreply.github.com>
2023-10-09 01:32:58 -0500

    feat:First version

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { Box } from "./components/Box/Box";
+export { DisplaySelector } from "./components/DisplaySelector/DisplaySelector";
+export { Flex } from "./components/Flex/Flex";
+export { Grid } from "./components/Grid/Grid";
+export { Typography } from "./components/Typography/Typography";
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import tsConfigPaths from "vite-tsconfig-paths";
+import * as packageJson from "./package.json";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(() => ({
+  plugins: [
+    react(),
+    tsConfigPaths(),
+    dts({
+      outdir: "dist",
+      include: ["src"],
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve("src", "index.ts"),
+      name: "sb-core-components",
+      formats: ["es", "cjs"],
+      fileName: (format) =>
+        `sb-core-components.${format === "cjs" ? "cjs" : "es.js"}`,
+    },
+    optimizeDeps: {
+      exclude: Object.keys(packageJson.peerDependencies),
+    },
+    esbuild: {
+      minify: true,
+    },
+  },
+}));
