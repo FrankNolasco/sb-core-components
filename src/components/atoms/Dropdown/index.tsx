7b0d6c90282fbca19a87eec4ae130d1b28d3afd2
Frank Nolasco Velasquez <49081234+FrankNolasco@users.noreply.github.com>
2023-10-10 01:05:17 -0500

    Fix: dropdown is the problem?

--- src/components/atoms/Dropdown/index.tsx
+++ src/components/atoms/Dropdown/index.tsx
@@ -1,4 +1,3 @@
-import Select, { SelectProps } from "react-dropdown-select";
-export const Dropdown = (props: SelectProps<object>) => {
-  return <Select {...props} />;
+export const Dropdown = () => {
+  return <div>Dropdown is the problem?</div>;
 };
